787f9e86a6ed83a80f1df56d20284a0e27fba6b3
Danish Awasthi <awasthidanish@gmail.com>
2023-03-23 09:15:51 +0530

    Added Options

--- Project.py
+++ Project.py
@@ -8,3 +8,16 @@ Minutes=[]
 Parked_time=[]
 bikes=50
 cars=50
+while True:
+    print("\n\n")
+    print("                Parking Management System")
+    print("﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌")
+    print("1.Vehicle Entry")
+    print("2.Remove Entry" )
+    print("3.View Parked Vehicle ")
+    print("4.View Available Parking Space ")
+    print("5.Parking charges ")
+    print("6.Bill")
+    print("7.Close Programme ")
+    print("\n")
+    opt=int(input())
